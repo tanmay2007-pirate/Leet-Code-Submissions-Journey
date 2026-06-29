899f708b8428d360038b56a25311141393562bb3
Tanmay Patharkar <tanmaypatharkar2007@gmail.com>
2026-06-29 20:56:55 +0530

    Time: 1 ms (99.98%), Space: 46.6 MB (91.72%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        
+
+        int result = 0;
+
+        for(int i = 0 ; i < nums.length ; i++){
+
+            result ^= nums[i];
+        }
+
+        return result;
+
+
+    }
+}
