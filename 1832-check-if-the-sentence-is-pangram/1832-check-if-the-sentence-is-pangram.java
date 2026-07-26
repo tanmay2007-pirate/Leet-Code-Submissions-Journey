3325f0d19259131e69c7761cd464c7b4065ffc96
Tanmay Patharkar <tanmaypatharkar2007@gmail.com>
2026-07-26 18:13:22 +0530

    Time: 1 ms (81.81%), Space: 43 MB (33.54%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -4,13 +4,14 @@ class Solution {
         boolean [] arr = new boolean[26];
 
         int n = sentence.length();
+        
 
         for(int i = 0 ; i < n ; i++ ){
 
             arr[sentence.charAt(i) - 'a'] = true;
         }
 
-        for(int i = 0 ; i < arr.length ; i++){
+        for(int i = 0 ; i < 26 ; i++){
             if(arr[i] == false){
                 return false;
             }
