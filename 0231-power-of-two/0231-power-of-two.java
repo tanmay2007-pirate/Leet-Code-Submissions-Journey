3e1de24e7f33eac0456b0871f0d0f580d068b78f
Tanmay Patharkar <tanmaypatharkar2007@gmail.com>
2026-08-07 20:19:05 +0530

    Time: 0 ms (100%), Space: 42.6 MB (56.44%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+        if(((n & (n-1)) == 0) && (n>0)) {
+            return true;
+        }else{
+            return false;
+        }
+
+        
+    }
+}
