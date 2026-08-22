172b8880aee14f12ce62ae97e6dc9799b9108de5
Tanmay Patharkar <tanmaypatharkar2007@gmail.com>
2026-08-22 19:19:27 +0530

    Time: 3 ms (93.75%), Space: 42.3 MB (90.45%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.java
+++ 2652-sum-multiples/2652-sum-multiples.java
@@ -1,29 +1,11 @@
 class Solution {
     public int sumOfMultiples(int n) {
-        
-        int sum = 0;
-        int lp = 1;
-        int rp = n;
-
-        while(rp>=lp){
-            if(lp%3 == 0 || lp%5 == 0 || lp%7==0){
-                sum+= lp;
-                
+       int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+                sum += i;
             }
-            if((rp%3 == 0 || rp%5 == 0 || rp%7 == 0) && rp != lp){
-                sum+= rp;
-            }
-            lp++;
-            rp--;
         }
-
-        // for(int i = 1 ; i <= n ; i++){
-        //     if(i%3 == 0 || i%5 == 0 || i%7 == 0){
-        //         sum+= i;
-        //     }
-        // }
-
-
         return sum;
     }
 }
